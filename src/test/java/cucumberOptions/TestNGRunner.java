9803f328b21e0331b0a450e9491e1b25a8deb92e
maheshshelke <shelke.mahendra@gmail.com>
2025-04-10 10:14:40 +0100

    commit 10/04

--- src/test/java/cucumberOptions/TestNGRunner.java
+++ src/test/java/cucumberOptions/TestNGRunner.java
@@ -7,7 +7,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", tags="@allProduct", monochrome = true, plugin = {
+@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", tags="@checkout", monochrome = true, plugin = {
 		"pretty", "html:target/cucumber-reports/cucumber.html", "json:target/cucumber-reports/cucumber.json"}, dryRun = false, publish = true, snippets = CucumberOptions.SnippetType.CAMELCASE
 )
 public class TestNGRunner extends AbstractTestNGCucumberTests {
